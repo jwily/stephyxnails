5c61c57a7f35c401e3acf83a0e69d862d64c4386
jwily <johnlee1120@gmail.com>
2023-10-23 16:55:28 -0700

    add rest package

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/orders/', include('orders.urls'))
+    path('api/', include('orders.urls'))
 ]
--- backend/orders/models.py
+++ backend/orders/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Order(models.Model):
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
-  name = models.CharField(max_length=127)
+  name = models.CharField(max_length=100)
   email = models.EmailField(max_length=254)
-  instagram = models.CharField(max_length=127, blank=True, default='')
+  instagram = models.CharField(max_length=100, blank=True, default='')
 
   class Meta:
     ordering = ['updated']
@@ -23,7 +23,7 @@ class Set(models.Model):
 class Tier(models.Model):
   price = models.IntegerField()
   description = models.TextField()
-  name = models.CharField(max_length=31)
+  name = models.CharField(max_length=30)
 
   class Meta:
     ordering = ['price']
