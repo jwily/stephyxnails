1b79da972a2d691fe6a74a04f616bde61d3a7841
jwily <johnlee1120@gmail.com>
2023-10-24 11:33:14 -0700

    orders json looking good

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -1,17 +1,19 @@
 from rest_framework import serializers
 from orders.models import Order, Set
 
-
-class OrderSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Order
-    # Edit these fields!
-    fields = ['id', 'name', 'email', 'instagram', 'created', 'updated', 'sets']
-
 class SetSerializer(serializers.ModelSerializer):
-  order = OrderSerializer(read_only=True)
+  # order = OrderSerializer(read_only=True)
 
   class Meta:
     model = Set
     # Edit these fields!
     fields = ['id', 'created', 'updated', 'description', 'order']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+  sets = SetSerializer(many=True, read_only=True)
+
+  class Meta:
+    model = Order
+    # Edit these fields!
+    fields = ['id', 'name', 'email', 'instagram', 'created', 'updated', 'sets']
