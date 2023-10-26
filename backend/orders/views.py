9e98c2960c14a5ef935f702fa1449defb1e93a30
jwily <johnlee1120@gmail.com>
2023-10-26 12:18:36 -0700

    added development catchall

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,8 +1,13 @@
+import urllib.request
+
+from rest_framework import generics
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.http import HttpResponse, StreamingHttpResponse
+from django.template import engines
+
 from orders.models import Order, Set, Tier, SetImage, ExampleImage
 from orders.serializers import OrderSerializer, SetSerializer, SetImageSerializer, ExampleImageSerializer, TierSerializer
-from rest_framework import generics
-from django.shortcuts import render
-from django.views.generic import TemplateView
 
 """
 This view associated with "orders/"
@@ -55,7 +60,69 @@ class SetImageDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = SetImage.objects.all()
     serializer_class = SetImageSerializer
 
-# def render_react(request):
-#     return render(request, "index.html")
+def iter_response(response, chunk_size=65536):
 
-catchall = TemplateView.as_view(template_name='index.html')
+    """
+    By default, when a response is read, it attempts
+    to read all of the content at once until the end.
+
+    This function will be used to break up a response
+    into small chunks (64 KB to be precise) for a smoother
+    development experience.
+
+    It will "yield" data in 64 KB chunks until all of the data is read.
+    "Yield" is cool because it's like return but the function does not end!
+    """
+
+    try:
+        while True:
+            data = response.read(chunk_size)
+            if not data:
+                break
+            yield data
+    finally:
+        response.close()
+
+def catchall_dev(request, upstream='http://localhost:3000'):
+
+    """
+    This function is meant to kick in during development when
+    navigating localhost:8000. Basically, if none of the routes
+    that we defined with Django REST Framework views are requested,
+    then the assumption is that front-end stuff is being requested
+    and so the application is asked to look at localhost:3000.
+    """
+
+    upstream_url = upstream + request.path
+    response = urllib.request.urlopen(upstream_url)
+    content_type = response.getheader('Content-Type')
+
+    """
+    If the content type is HTML, then it will be read and served
+    using Django's templating system.
+
+    If it isn't, it must be some other type of static resource, which
+    will be broken up into chunks using the function from above.
+    """
+
+    if content_type == 'text/html; charset=UTF-8':
+        response_text = response.read().decode()
+        response.close()
+        return HttpResponse(
+            engines['django'].from_string(response_text).render(),
+            content_type=content_type,
+            status=response.status,
+            reason=response.reason,
+        )
+
+    else:
+        return StreamingHttpResponse(
+            iter_response(response),
+            content_type=content_type,
+            status=response.status,
+            reason=response.reason,
+        )
+
+catchall_prod = TemplateView.as_view(template_name='index.html')
+
+catchall = catchall_dev if settings.DEBUG else catchall_prod
