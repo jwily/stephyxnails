effd3eb947c4a01be2fecdbe81aac702a8f741c3
jwily <johnlee1120@gmail.com>
2023-10-26 10:34:50 -0700

    turn debug off again

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 # For ease of use in development
 ALLOWED_HOSTS = ['localhost']
