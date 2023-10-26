94f2b7d4330205f5cabe077252bac09ff9aa03da
jwily <johnlee1120@gmail.com>
2023-10-25 18:52:14 -0700

    tweak settings

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -27,7 +27,7 @@ FRONTEND_DIR = BASE_DIR.parent / 'frontend'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='supersecretpleasedontsteal')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
