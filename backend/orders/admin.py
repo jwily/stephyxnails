41f0c6f332887d48ee9d44dd8ae55ac3eb79217b
jwily <johnlee1120@gmail.com>
2023-10-27 13:37:24 -0700

    add order actions to admin

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -12,32 +12,37 @@ class SetInline(admin.TabularInline):
         models.TextField: {'widget': Textarea(attrs={'rows':5, 'cols':60})},
     }
 
+
 class SetAdmin(admin.ModelAdmin):
   list_display = ['order', 'short_description', 'shape', 'sizes', 'tier', 'created', 'updated']
 
+
 class OrderAdmin(admin.ModelAdmin):
   inlines = [SetInline]
-  list_display = ['id', 'name', 'email', 'instagram', 'status', 'created', 'updated']
-  actions=[]
+  list_display = ['__str__', 'name', 'email', 'instagram', 'status', 'created', 'updated']
 
-  @admin.action(description="Mark selected orders as pending")
+  @admin.action(description="Mark selected as completed")
+  def make_completed(self, request, queryset):
+      queryset.update(status="completed")
+
+  @admin.action(description="Mark selected as pending")
   def make_pending(self, request, queryset):
-    queryset.update(status='pending')
+      queryset.update(status="pending")
 
-  @admin.action(description="Mark selected orders as in progress")
-  def make_completed(self, request, queryset):
-    queryset.update(status='in_progress')
+  @admin.action(description="Mark selected as in progress")
+  def make_in_progress(self, request, queryset):
+      queryset.update(status="in_progress")
 
-  @admin.action(description="Mark selected orders as completed")
-  def make_completed(self, request, queryset):
-    queryset.update(status='completed')
+  @admin.action(description="Mark selected as canceled")
+  def make_canceled(self, request, queryset):
+      queryset.update(status="canceled")
 
-  @admin.action(description="Mark selected orders as canceled")
-  def make_completed(self, request, queryset):
-    queryset.update(status='canceled')
+  actions = ['make_pending', 'make_in_progress', 'make_completed', 'make_canceled']
+
 
 class TierAdmin(admin.ModelAdmin):
   list_display = ['name', 'price', 'description']
+
 
 admin.site.register(Order, OrderAdmin)
 admin.site.register(Tier, TierAdmin)
