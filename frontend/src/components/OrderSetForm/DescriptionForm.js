5fd3abe958d7d9c5641b8011c4ea70770d326a7e
JustineJang <justinejang415@gmail.com>
2023-10-30 18:47:06 -0700

    validations

--- frontend/src/components/OrderSetForm/DescriptionForm.js
+++ frontend/src/components/OrderSetForm/DescriptionForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useRef} from 'react';
 import { useHistory } from 'react-router-dom';
 import { useOrderContext } from '../../context/OrderContext';
 
@@ -9,15 +9,24 @@ function DescriptionFrom() {
   const history = useHistory() 
 
     const { formData, updateFormData } = useOrderContext();
+    const errorRef = useRef(null);
 
     const handleNext = (e) => {
       e.preventDefault()
+      if (formData.description && formData.description.length <= 5000) {
       // Add the data to the current data set
       updateFormData({ description: formData.description});
       console.log('update', formData)
 
       history.push('/order-set/extra'); // Navigate to the next form question
-    };
+      errorRef.current.innerText = ''; // Clear any previous error message
+
+    }  else {
+      // Description is empty or too long; show an error message
+      errorRef.current.innerText =
+        'Please provide a description (up to 5000 characters) before proceeding.';
+    }
+  };
 
     const handleBack = () => {
       // Navigate back to the previous step
@@ -43,6 +52,8 @@ function DescriptionFrom() {
           </div>
 
           <div>
+          <div ref={errorRef} style={{ color: 'red' }}></div>
+
             <button onClick={handleBack}>Back</button>
             <button onClick={handleNext}>Next</button>
           </div>
--- frontend/src/components/OrderSetForm/ExtraForm.js
+++ frontend/src/components/OrderSetForm/ExtraForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useHistory } from 'react-router-dom';
 import { useOrderContext } from '../../context/OrderContext';
 
@@ -6,46 +6,56 @@ import { useOrderContext } from '../../context/OrderContext';
 function ExtraForm() {
 
     const history = useHistory() 
-
     const { formData, updateFormData } = useOrderContext();
+    const [inputValue, setInputValue] = useState(formData.extra);
+    const [calculatedValue, setCalculatedValue] = useState(0);
+
 
     const handleNext = (e) => {
-        e.preventDefault()
-      // Add the data to the current data set\
-      updateFormData({ extra: formData.extra});
-      console.log('update', formData)
-
-
-      history.push('/order-set/all'); // Navigate to the next form question
-    };
-
-
-      const handleBack = () => {
-        
-        // Navigate back to the previous step
-        history.push('/order-set/description'); // Replace 'previous-step-url' with the actual URL for the previous step
+        e.preventDefault();
+        // Calculate the value as number * 3
+        const calculated = inputValue * 3;
+        // Update the form data with the input value
+        updateFormData({ extra: inputValue });
+        setCalculatedValue(calculated); // Update the calculated value
+        history.push('/order-set/all'); // Navigate to the next form question
       };
+    
+      const handleBack = () => {
+        // Navigate back to the previous step
+        history.push('/order-set/description');
+      };
+    
+      const handleInputChange = (e) => {
+        const newValue = parseInt(e.target.value);
+        if (!isNaN(newValue)) {
+          // Ensure the input is a valid integer
+          setInputValue(newValue);
+          // Calculate the new value and update the calculatedValue
+          setCalculatedValue(newValue * 3);
+        }
+      };
+
     
   
     return (
         <>
         <section> 
-            <h2>5.Charms</h2>
-                <p>disclaimer insert</p>
-                    <div>
-                        <textarea
-                            className="#"
-                            type="text"
-                            placeholder=""
-                            value={formData.extra}
-                            onChange={ (e) =>  updateFormData({ extra: e.target.value })}>     
-                        </textarea>
-                    </div> 
-                <div>
-                    {/* <button onClick={() => history.goBack()}>Go Back</button> */}
-                    <button onClick={handleBack}>Back</button>
-                    <button onClick={handleNext}>Next</button>
-                </div>
+        <h2>5. Charms</h2>
+        <p>disclaimer insert</p>
+        <div>
+          <input
+            type="number"
+            placeholder=""
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <p>Calculated: {calculatedValue}</p>
+        </div>
+        <div>
+          <button onClick={handleBack}>Back</button>
+          <button onClick={handleNext}>Next</button>
+        </div>
         </section>
         </>
     );
