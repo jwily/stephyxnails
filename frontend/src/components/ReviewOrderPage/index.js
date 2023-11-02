68e06c4ddf05e3c9acf1ef962d2dfbf6e3553e74
JustineJang <justinejang415@gmail.com>
2023-11-02 12:26:42 -0700

    edit works

--- frontend/src/components/ReviewOrderPage/index.js
+++ frontend/src/components/ReviewOrderPage/index.js
@@ -5,36 +5,39 @@ import ReCAPTCHA from "react-google-recaptcha"
 
 const ReviewOrderPage = () => {
 
-const history = useHistory();
-
-  const { state, dispatch } = useOrderContext();
-  const { sets } = state;
+    const history = useHistory();
+    const { state, dispatch } = useOrderContext();
+    const { sets } = state;
 
     // Define state variables for edited user information
     const [editedName, setEditedName] = useState(state.name);
     const [editedEmail, setEditedEmail] = useState(state.email);
     const [editedInstagram, setEditedInstagram] = useState(state.instagram);
   
-      // Define edit mode flags for each field
-  const [isEditingName, setIsEditingName] = useState(false);
-  const [isEditingEmail, setIsEditingEmail] = useState(false);
-  const [isEditingInstagram, setIsEditingInstagram] = useState(false);
-  const handleSaveUserInformation = () => {
-    // Create an object with the edited user information
-    const updatedUserInformation = {
-      name: editedName,
-      email: editedEmail,
-      instagram: editedInstagram,
+    // Define edit mode flags for each field
+    const [isEditingName, setIsEditingName] = useState(false);
+    const [isEditingEmail, setIsEditingEmail] = useState(false);
+    const [isEditingInstagram, setIsEditingInstagram] = useState(false);
+    
+    const handleSaveUserInformation = () => {
+        // Create an object with the edited user information
+        const updatedUserInformation = {
+        name: editedName,
+        email: editedEmail,
+        instagram: editedInstagram,
+        };
+  
+        // Update the user information in your state
+        dispatch({ type: 'UPDATE_USER_INFORMATION', payload: updatedUserInformation });
+  
+       
+        // When isEditingName is true, an input field is displayed for editing the name. 
+        // The onChange event handler captures the input changes and updates the editedName stat
+        // When isEditingName is false, the name is displayed as plain text, and a "Edit" button allows users to switch to edit mode.
+        setIsEditingName(false);
+        setIsEditingEmail(false);
+        setIsEditingInstagram(false);
     };
-  
-    // Update the user information in your state
-    dispatch({ type: 'UPDATE_USER_INFORMATION', payload: updatedUserInformation });
-  
-    // Exit edit mode
-    setIsEditingName(false);
-    setIsEditingEmail(false);
-    setIsEditingInstagram(false);
-  };
 
 
     const handleBack = () => {
@@ -80,86 +83,108 @@ const history = useHistory();
     const recaptchaRef = React.createRef();
 
     return (
-      <div>
-      <h2>Review Your Order</h2>
-      <div>
-        <h3>User Information</h3>
-        {isEditingName ? (
     <div>
-      <label>Name: </label>
-      <input
-        type="text"
-        value={editedName}
-        onChange={(e) => setEditedName(e.target.value)} // Handle input change
-      />
-      <button onClick={() => setIsEditingName(false)}>Cancel</button>
-      <button onClick={handleSaveUserInformation}>Save</button>
-    </div>
-  ) : (
-    <div>
-      <p>Name: {state.name}</p>
-      <button onClick={() => setIsEditingName(true)}>Edit</button>
-    </div>
-  )}
+            <div>
+                <h2>Review Your Entire Order</h2>
+            </div>
 
-{isEditingEmail ? (
-    <div>
-      <label>Email: </label>
-      <input
-        type="text"
-        value={editedEmail}
-        onChange={(e) => setEditedEmail(e.target.value)} // Handle input change
-      />
-      <button onClick={() => setIsEditingEmail(false)}>Cancel</button>
-      <button onClick={handleSaveUserInformation}>Save</button>
-    </div>
-  ) : (
-    <div>
-      <p>Email: {state.email}</p>
-      <button onClick={() => setIsEditingEmail(true)}>Edit</button>
-    </div>
-  )}
+        <div>
+            <h3>User Information</h3>
+            <section>
+            {isEditingName ? (
+                <div>
+                    <label>Name: </label>
+                    <input
+                        type="text"
+                        value={editedName}
+                        onChange={(e) => setEditedName(e.target.value)} // Handle input change
+                    />
+                    <div>
+                        <button onClick={() => setIsEditingName(false)}>Cancel</button>
+                        <div></div>
+                        <button onClick={handleSaveUserInformation}>Save</button>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <p>Name: {state.name}</p>
+                    <button onClick={() => setIsEditingName(true)}>Edit</button>
+                </div>
+            )}
+            </section>
+            <section>
+            {isEditingEmail ? (
+                <div>
+                    <label>Email: </label>
+                    <input
+                        type="text"
+                        value={editedEmail}
+                        onChange={(e) => setEditedEmail(e.target.value)} // Handle input change
+                    />
+                    <div>
+                        <button onClick={() => setIsEditingEmail(false)}>Cancel</button>
+                        <div></div>
+                        <button onClick={handleSaveUserInformation}>Save</button>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <p>Email: {state.email}</p>
+                    <button onClick={() => setIsEditingEmail(true)}>Edit</button>
+                </div>
+            )}
+            </section>
+            <section>
+            {isEditingInstagram ? (
+                <div>
+                    <label>Instagram: </label>
+                    <input
+                        type="text"
+                        value={editedInstagram}
+                        onChange={(e) => setEditedInstagram(e.target.value)} // Handle input change
+                    />
+                    <div>
+                        <button onClick={() => setIsEditingInstagram(false)}>Cancel</button>
+                        <div></div>
+                        <button onClick={handleSaveUserInformation}>Save</button>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <p>Instagram: {state.instagram}</p>
+                    <button onClick={() => setIsEditingInstagram(true)}>Edit</button>
+                </div>
+            )}
+            </section>
+        </div>
 
-{isEditingInstagram ? (
-    <div>
-      <label>Instagram: </label>
-      <input
-        type="text"
-        value={editedInstagram}
-        onChange={(e) => setEditedInstagram(e.target.value)} // Handle input change
-      />
-      <button onClick={() => setIsEditingInstagram(false)}>Cancel</button>
-      <button onClick={handleSaveUserInformation}>Save</button>
-    </div>
-  ) : (
-    <div>
-      <p>Insta: {state.instagram}</p>
-      <button onClick={() => setIsEditingInstagram(true)}>Edit</button>
-    </div>
-  )}
-      </div>
-      <div>
-        <h3>Order Sets</h3>
-      
-{sets.map((formData, index) => {
-          return(
-          <li key={index}>
-            <h4>Set {index + 1}</h4>
-            <p>Tier: {formData.tier}</p>
-            <p>Shape: {formData.shape}</p>
-            <p>photo: {formData.photo}</p>
-            <p>Description: {formData.description}</p>
-            <p>Extra: {formData.extra}</p>
-            <button onClick={() => handleDeleteSet(index)}>Delete Set</button>
-          </li>
-          )}
-        )}
+        <div>
+            <section>
+                {sets.map((formData, index) => {
+                return(
+                    <li key={index}>
+                    <h4>Set {index + 1}</h4>
+                    <p>Tier: {formData.tier}</p>
+                    <p>Shape: {formData.shape}</p>
+                    <p>photo: {formData.photo}</p>
+                    <p>Description: {formData.description}</p>
+                    <p>Extra: {formData.extra}</p>
+                    <button onClick={() => handleDeleteSet(index)}>Delete Set</button>
+                </li>
+                )}
+                )}
+            </section>
+        </div>
 
-      </div>
-      <button onClick={handleBack}>Back</button>
-      <button onClick={handleSubmit}>Submit Order</button>
+        <div>
+            <div>
+                <button onClick={handleBack}>Back</button>
+            </div>
+            <div>
+                <button onClick={handleSubmit}>Submit Order</button>
+            </div>
+        </div>
     </div>
-
 
         // {/* {info.sets.map(set =>
         //     <div key={set.description.length}>
@@ -179,30 +204,29 @@ const history = useHistory();
 
         //     {/* <button onClick={handleSubmit}>Submit</button> */}
 
-
-    )
-    return (
-        <div>
-            {/* {info.sets.map(set =>
-                <div key={set.description.length}>
-                    <div>
-                        {set.description}
-                    </div>
-                    <div>
-                        {set.shape}
-                    </div>
-                    <div>
-                        {set.size}
-                    </div>
-                    <div>
-                        {set.tier}
-                    </div>
-                </div>)} */}
-            {/* <div className="g-recaptcha" data-sitekey="6Ld2fOEoAAAAABOW9mr23wNIcTakNByHf5ArjqdW"></div> */}
-            <ReCAPTCHA ref={recaptchaRef} sitekey="6Ld2fOEoAAAAABOW9mr23wNIcTakNByHf5ArjqdW" onChange={handleSubmit} />
-            <button onClick={handleSubmit}>Submit</button>
-        </div>
-    )
+    );
+    // return (
+    //     <div>
+    //         {/* {info.sets.map(set =>
+    //             <div key={set.description.length}>
+    //                 <div>
+    //                     {set.description}
+    //                 </div>
+    //                 <div>
+    //                     {set.shape}
+    //                 </div>
+    //                 <div>
+    //                     {set.size}
+    //                 </div>
+    //                 <div>
+    //                     {set.tier}
+    //                 </div>
+    //             </div>)} */}
+    //         {/* <div className="g-recaptcha" data-sitekey="6Ld2fOEoAAAAABOW9mr23wNIcTakNByHf5ArjqdW"></div> */}
+    //         <ReCAPTCHA ref={recaptchaRef} sitekey="6Ld2fOEoAAAAABOW9mr23wNIcTakNByHf5ArjqdW" onChange={handleSubmit} />
+    //         <button onClick={handleSubmit}>Submit</button>
+    //     </div>
+    // )
 }
 
 
--- frontend/src/context/OrderContext.js
+++ frontend/src/context/OrderContext.js
@@ -25,12 +25,15 @@ const reducer = (state=initialState, action) => {
     case 'SET_NAME':
       console.log("Setting name to:", action.payload);
       return { ...state, name: action.payload };
+
     case 'SET_EMAIL':
       console.log("Setting email to:", action.payload);
       return { ...state, email: action.payload };
+
     case 'SET_INSTAGRAM':
       console.log("Setting Instagram to:", action.payload);
       return { ...state, instagram: action.payload };
+
     case 'UPDATE_FORM_DATA':
       console.log("Updating form data with:", action.payload);
       return { ...state, formData: { ...state.formData, ...action.payload } };
@@ -45,28 +48,34 @@ const reducer = (state=initialState, action) => {
         description: '',
         extra: '' }
       return newState;
-      case 'ADD_SET':
-        // Add a new set and increment setCount
-        const newSets = [...state.sets, action.payload];
-        return { ...state, sets: newSets, setCount: state.setCount + 1 };
-      case 'INITIALIZE_STATE':
-        // Load initial state from localStorage if it exists
-        const savedState = localStorage.getItem('orderState');
-        return savedState ? JSON.parse(savedState) : initialState;
-      case 'SAVE_STATE':
-        // Save the state to localStorage
-        localStorage.setItem('orderState', JSON.stringify(state));
-        return state;   
-        case 'CLEAR_LOCAL_STORAGE':
-        // Clear the localStorage
-        localStorage.clear();
-        return { ...initialState }; // Reset state to its initial state
-        case 'DELETE_SET':
+
+    case 'ADD_SET':
+       // Add a new set and increment setCount
+      const newSets = [...state.sets, action.payload];
+      return { ...state, sets: newSets, setCount: state.setCount + 1 };
+
+    case 'INITIALIZE_STATE':
+      // Load initial state from localStorage if it exists
+      const savedState = localStorage.getItem('orderState');
+      return savedState ? JSON.parse(savedState) : initialState;
+
+    case 'SAVE_STATE':
+      // Save the state to localStorage
+      localStorage.setItem('orderState', JSON.stringify(state));
+      return state;  
+      
+    case 'CLEAR_LOCAL_STORAGE':
+      // Clear the localStorage
+      localStorage.clear();
+      return { ...initialState }; // Reset state to its initial state
+    
+    case 'DELETE_SET':
       // Remove a set at the specified index
       const indexToDelete = action.payload;
       const updatedSets = [...state.sets.slice(0, indexToDelete), ...state.sets.slice(indexToDelete + 1)];
       return { ...state, sets: updatedSets, setCount: state.setCount - 1 };
-      case 'UPDATE_USER_INFORMATION':
+
+    case 'UPDATE_USER_INFORMATION':
       // Update user information fields based on the payload
       return {
         ...state,
@@ -75,10 +84,9 @@ const reducer = (state=initialState, action) => {
         instagram: action.payload.instagram,
       };
 
-  
-    // case 'CLEAR_FORM':
-    //         console.log("Clearing the form");
-    //   return { ...state, formData: { tier: '', shape: '', photo: [], description: '', extra: '' } };
+  //case 'CLEAR_FORM':
+    //return { ...state, formData: { tier: '', shape: '', photo: [], description: '', extra: '' } };
+    
     default:
       return state;
   }
@@ -94,10 +102,10 @@ export const OrderProvider = ({ children }) => {
       dispatch({ type: 'INITIALIZE_STATE' });
     }, []);
   
-  // Save the state to localStorage whenever it changes
-  useEffect(() => {
-    dispatch({ type: 'SAVE_STATE' });
-  }, [state]);
+    // Save the state to localStorage whenever it changes
+    useEffect(() => {
+      dispatch({ type: 'SAVE_STATE' });
+    }, [state]);
 
 
   const scrollToOrder = useRef()
