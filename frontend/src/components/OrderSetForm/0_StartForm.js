df2414f56f54fcc193396cffc3c3b89bb333d47c
JustineJang <justinejang415@gmail.com>
2023-11-02 00:56:17 -0700

    start form

--- frontend/src/components/OrderSetForm/0_StartForm.js
+++ frontend/src/components/OrderSetForm/0_StartForm.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 function StartForm( ) {
         
+  const [showResetWarning, setShowResetWarning] = useState(false);
+
     const handleBack = () => {
+      if (showResetWarning) {
+
         // Navigate back to the previous step
         window.location.href ='/order'; // Replace 'previous-step-url' with the actual URL for the previous step
+      } else {
+        setShowResetWarning(true);
       };
-    
+    }
+
+    const handleCancelReset = () => {
+      setShowResetWarning(false);
+    };
+
     return (
         <div>
           <h1>It sounds like you're ready to start building custom nail sets!</h1>
@@ -16,8 +27,21 @@ function StartForm( ) {
             <button>Start Form</button>
           </Link>
           <div>
-          <button onClick={handleBack}>Back</button>
+        {showResetWarning ? (
+          <div>
+            <p style={{ color: 'red' }}>
+              Going back will reset your form. Are you sure you want to proceed?
+            </p>
+            <div>
+            <button onClick={handleBack}>Yes, I want to proceed</button>
+
+            </div>
+            <button onClick={handleCancelReset}>No, I don't want to proceed</button>
           </div>
+        ) : (
+          <button onClick={handleBack}>Back</button>
+        )}
+      </div>
         </div>
       );
 }
