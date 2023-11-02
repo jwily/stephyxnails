4149d1363baee51a03a9ea1c6af04b9e0613c0a6
JustineJang <justinejang415@gmail.com>
2023-11-01 17:26:29 -0700

    context

--- frontend/src/context/OrderContext.js
+++ frontend/src/context/OrderContext.js
@@ -30,7 +30,6 @@ const reducer = (state=initialState, action) => {
     case 'UPDATE_FORM_DATA':
       console.log("Updating form data with:", action.payload);
       return { ...state, formData: { ...state.formData, ...action.payload } };
-<<<<<<< HEAD
     case 'SAVE_FORM_DATA':
       const newState = {...state};
       newState.sets = [...newState.sets, newState.formData];
@@ -46,23 +45,12 @@ const reducer = (state=initialState, action) => {
     // case 'CLEAR_FORM':
     //         console.log("Clearing the form");
     //   return { ...state, formData: { tier: '', shape: '', photo: [], description: '', extra: '' } };
-=======
-    case 'UPDATE_MERGED_DATA':
-      return { ...state, mergedData: action.payload };
-    case 'SAVE_FORM_DATA':
-      console.log("Saving form data:", state.formData);
-      return { ...state, formDataSets: [...state.formDataSets, state.formData] };
-    case 'CLEAR_FORM':
-      console.log("Clearing the form");
-      return { ...state, formData: { tier: '', shape: '', photo: [], description: '', extra: '' } };
->>>>>>> staging
     default:
       return state;
   }
 };
 
 export const OrderProvider = ({ children }) => {
-<<<<<<< HEAD
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
@@ -70,53 +58,6 @@ export const OrderProvider = ({ children }) => {
 
   return (
     <OrderContext.Provider value={{ state, dispatch }}>
-=======
-  const [formDataSets, setFormDataSets] = useState([]);
-  const [mergedData, setMergedData] = useState([]);
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [instagram, setInstagram] = useState('');
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const [formData, setFormData] = useState({
-    tier: '',
-    shape: '',
-    photo: '',
-    description: '',
-    extra: '',
-  });
-  const scrollToOrder = useRef()
-  const scrollToAbout = useRef()
-  const scrollToGallery = useRef()
-  const scrollToFAQ = useRef()
-
-  const updateFormData = (newData) => {
-    setFormData({ ...formData, ...newData });
-  };
-
-  // Update mergedData whenever formDataSets and formData change
-  useEffect(() => {
-    setMergedData([...formDataSets, formData]);
-  }, [formDataSets, formData]);
-
-  const saveCurrentDataSet = () => {
-    setFormDataSets([...formDataSets, formData]);
-    setMergedData([...formDataSets, formData]);
-  };
-
-  const clearForm = () => {
-    setFormData({  // Clear the form fields
-      tier: '',
-      shape: '',
-      photo: '',
-      description: '',
-      extra: '',
-    });
-  };
-
-
-  return (
-    <OrderContext.Provider value={{ state, dispatch, name, setName, email, setEmail, instagram, setInstagram, scrollToAbout, scrollToFAQ, scrollToGallery, scrollToOrder, formData, updateFormData, formDataSets, saveCurrentDataSet, clearForm, mergedData }}>
->>>>>>> staging
       {children}
     </OrderContext.Provider>
   );
