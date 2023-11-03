c4e1c066bae1b0ea723593acefc8992084c3a973
JustineJang <justinejang415@gmail.com>
2023-11-02 23:10:46 -0700

    sub form load

--- frontend/src/components/OrderSetForm/6_SubmissionForm.js
+++ frontend/src/components/OrderSetForm/6_SubmissionForm.js
@@ -3,119 +3,90 @@ import { useOrderContext } from '../../context/OrderContext';
 import { useHistory } from 'react-router-dom';
 
 function SubmissionSetForm() {
-
+  // Initialize the history object and retrieve state and dispatch from the order context
   const history = useHistory();
   const { state, dispatch } = useOrderContext();
-
-  const { sets, formData, setCount } = state;
-  const isOrderDetailsComplete = state.name && state.email && state.instagram;
+  const { formData, setCount } = state;  // Destructure values from the state
+  const isOrderDetailsComplete = state.name && state.email && state.instagram; // Check if order details are complete
   const [isLoading, setIsLoading] = useState(true); // Initialize the loading state
-
+  
   useEffect(() => {
-    // Simulate loading for 2 seconds (adjust as needed)
+    // Simulate loading for 100 milliseconds (0.1 seconds) and then set loading to false
     setTimeout(() => {
       setIsLoading(false); // Set loading to false after the delay
-    }, 100); 
-
+    }, 100);  
     // Add dependencies as needed
   }, []);
+
   const redirectToOrderDetails = () => {
+    // Redirect the user to the '/order' page
     window.location.href ='/order'
   }
 
-  console.log(sets, "set")
-
   const handleSubmit = () => {
-
+    // Dispatch an action to save the form data and navigate to the '/review-order' page
     dispatch({ type: 'SAVE_FORM_DATA' });
     history.push('/review-order');
   };
 
   const handleAddAnotherSet = () => {
-  
+      // Dispatch actions to add the current set's data, clear the form, and navigate to '/order-set/tier'
       dispatch({ type: 'ADD_SET', payload: formData });
       dispatch({ type: 'CLEAR_FORM' });
-
       history.push('/order-set/tier');
   };
 
-  const handleBack = () => {
-      
-    // Navigate back to the previous step
-    history.push('/order-set/extra'); // Replace 'previous-step-url' with the actual URL for the previous step
+  const handleBack = () => { 
+    // Navigate back to the previous step, in this case, '/order-set/extra'
+    history.push('/order-set/extra'); 
   };
-
-
-
-
 
   return (
     <>
-       {isLoading ? ( // Display loading indicator while isLoading is true
-        <div>Loading...</div>
-      ) : (
-        <>
-
+    {isLoading ? ( // Display loading indicator while isLoading is true
+       <div>Loading...</div>
+    ) : (
+       <>
       {isOrderDetailsComplete ? (
         <>
         <div>
-         
-         <h2>Sets</h2>
 
-         <p>Number of Sets Made: {setCount}</p> {/* Display the set count */}
+          <h2>Sets</h2>
+          <p>Number of sets made: {setCount + 1}</p> {/* Display the set count */}
 
-      <div>
-        {/* <h3>Order Sets</h3>
-        {sets.map((formData, index) => {
-          return(
-          <li key={index}>
-            <h4>Set {index + 1}</h4>
+          <div>
+            <p> your current set</p>
             <p>Tier: {formData.tier}</p>
-            <p>Shape: {formData.shape}</p>
+            <p>shape: {formData.shape}</p>
             <p>photo: {formData.photo}</p>
-            <p>Description: {formData.description}</p>
-            <p>Extra: {formData.extra}</p>
-          </li>
-          )}
-        )} */}
-        <div>
-          <p> current set</p>
-          <p>Tier: {formData.tier}</p>
-          <p>shape: {formData.shape}</p>
-          <p>photo: {formData.photo}</p>
-          <p>description: {formData.description}</p>
-          <p>extra: {formData.extra}</p>
-
+            <p>description: {formData.description}</p>
+            <p>extra: {formData.extra}</p>
+          </div>
+          
+          <div>
+            <div>
+              <button onClick={handleBack}>Back</button>
+            </div>
+            <div>
+              <button type="submit" onClick={handleAddAnotherSet}>Add Another Set  </button>
+            </div>
+            <div>
+              <button type="submit" onClick={handleSubmit}>Submit</button>
+            </div>
+          </div>
 
         </div>
-      </div> 
-  
-      
-    {/* {sets.length === 0 && (
-      <p>No sets made.</p>
-    )} */}
-      <div>
-        <button onClick={handleBack}>Back</button>
-        <button type="submit" onClick={handleAddAnotherSet}>Add Another Set  </button>
-        <button type="submit" onClick={handleSubmit}>Submit</button>
-      </div>
-
-      </div>
-      </>
-                
-        
-        ) : (
-          <div>
+        </>           
+       ) : (
+        <div>
             <p>Please complete your order details before proceeding.</p>
             <button onClick={redirectToOrderDetails}>Complete Order Details</button>
-          </div>
-        )}
-     
+        </div>
+      )}  
       </>
-      )
-      }
-     </>
-
+    )
+    }
+    </>
   );
 }
 
