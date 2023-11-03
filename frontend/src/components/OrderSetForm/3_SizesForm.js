e48b6b7150698210a59b650ae23b3a18573ea882
JustineJang <justinejang415@gmail.com>
2023-11-03 14:47:47 -0700

    finger validation

--- frontend/src/components/OrderSetForm/3_SizesForm.js
+++ frontend/src/components/OrderSetForm/3_SizesForm.js
@@ -12,6 +12,14 @@ function SizesForm() {
   const [rightDisplay, setRightDisplay] = useState(['', '', '', '', '']);
   const [leftText, setLeftText] = useState('');
   const [rightText, setRightText] = useState('');
+  const [errorLeftHand, setErrorLeftHand] = useState('');
+  const [errorRightHand, setErrorRightHand] = useState('');
+  const [error, setError] = useState('');
+
+  const [leftHandIndexCompletion, setLeftHandIndexCompletion] = useState([false, false, false, false, false]);
+  const [rightHandIndexCompletion, setRightHandIndexCompletion] = useState([false, false, false, false, false]);
+
+
 
   const isOrderDetailsComplete = state.name && state.email 
   const [isLoading, setIsLoading] = useState(true); // Initialize the loading state
@@ -32,12 +40,22 @@ function SizesForm() {
   const handleNext = (e) => {
     e.preventDefault();
 
-    const left_sizes = leftDisplay.slice(0, 4).join('');
-    const right_sizes = rightDisplay.slice(0, 4).join('');
+    const isLeftHandValid = validateHand(leftDisplay);
+    const isRightHandValid = validateHand(rightDisplay);
+    const isBothHandsValid = isLeftHandValid && isRightHandValid;
+  
+    if (!isBothHandsValid) {
+      setError("Please provide input for all finger display indices in both hands.");
+    } else {
+      setError(""); // Clear the error message if both hands are valid
+      dispatch({ type: 'UPDATE_FORM_DATA', payload: { leftDisplay, rightDisplay } });
+      history.push('/order-set/photo'); // Navigate to the next form question
+    }
+  }
 
-    dispatch('ACTION_HERE');
-
-    history.push('/order-set/photo') // Navigate to the next form question
+  const validateHand = (handDisplay) => {
+    const pattern = /^\d{5}$/;
+    return pattern.test(handDisplay.join(''));
   };
 
   const handleBack = () => {
@@ -45,15 +63,13 @@ function SizesForm() {
     history.push('/order-set/shape');
   };
 
-  const FingerDisplay = ({ hand, name, idx }) => {
-
+  const FingerDisplay = ({ hand, name, value }) => {
     const valuesArray = hand === 'Left' ? leftDisplay : rightDisplay;
-
-    const value = valuesArray[parseInt(idx)];
 
     return (
       <div>
         {`${hand} ${name}: ${value ? value : ''}`}
+        
       </div>
     )
   }
@@ -62,8 +78,19 @@ function SizesForm() {
 
     const value = e.target.value;
 
+    const sanitizedValue = value.replace(/[^0-9 ,\-]/g, '');
+
+      // Check if the input contains invalid characters
+  if (sanitizedValue !== value) {
+    setError("Invalid characters detected. Only numbers, spaces, commas, and hyphens are allowed.");
+  } else {
+    setError(""); // Reset the error message if input is valid
+  }
+
     // Allows input to function correctly
-    setText(value);
+    setText(sanitizedValue);
+
+   
 
     // Code below parses through the text
     // and picks out valid numbers.
@@ -109,18 +136,18 @@ function SizesForm() {
         <p>disclaimer insert</p>
         <div>
           <p className='flex flex-row space-x-5'>
-            <FingerDisplay hand='Left' name='Pinky' idx='4' />
-            <FingerDisplay hand='Left' name='Ring' idx='3' />
-            <FingerDisplay hand='Left' name='Middle' idx='2' />
-            <FingerDisplay hand='Left' name='Index' idx='1' />
-            <FingerDisplay hand='Left' name='Thumb' idx='0' />
+          <FingerDisplay hand='Left' name='Pinky' value={leftDisplay[4]} />
+  <FingerDisplay hand='Left' name='Ring' value={leftDisplay[3]} />
+  <FingerDisplay hand='Left' name='Middle' value={leftDisplay[2]} />
+  <FingerDisplay hand='Left' name='Index' value={leftDisplay[1]} />
+  <FingerDisplay hand='Left' name='Thumb' value={leftDisplay[0]} />
           </p>
           <p className='flex flex-row space-x-5'>
-            <FingerDisplay hand='Right' name='Thumb' idx='0' />
-            <FingerDisplay hand='Right' name='Middle' idx='2' />
-            <FingerDisplay hand='Right' name='Index' idx='1' />
-            <FingerDisplay hand='Right' name='Ring' idx='3' />
-            <FingerDisplay hand='Right' name='Pinky' idx='4' />
+          <FingerDisplay hand='Right' name='Thumb' value={rightDisplay[0]} />
+  <FingerDisplay hand='Right' name='Middle' value={rightDisplay[2]} />
+  <FingerDisplay hand='Right' name='Index' value={rightDisplay[1]} />
+  <FingerDisplay hand='Right' name='Ring' value={rightDisplay[3]} />
+  <FingerDisplay hand='Right' name='Pinky' value={rightDisplay[4]} />
           </p>
         </div>
         <div>
@@ -131,15 +158,25 @@ function SizesForm() {
             type='text'
             value={leftText}
             onChange={(e) => textToDisplay(e, setLeftText, setLeftDisplay)}
+
           />
           <p>
+          {error && <div className="error-message">{error}</div>}
+          {errorLeftHand && <div className="error-message">{errorLeftHand}</div>}
+
+
             Right Hand
           </p>
           <input
             type='text'
             value={rightText}
             onChange={(e) => textToDisplay(e, setRightText, setRightDisplay)}
+
           />
+          {error && <div className="error-message">{error}</div>}
+          {errorRightHand && <div className="error-message">{errorRightHand}</div>}
+
+
         </div>
         <div>
           <button onClick={handleBack}>Back</button>
