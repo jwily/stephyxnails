efa9e617dd08aa57d3096246061e5a3561d9f388
jwily <johnlee1120@gmail.com>
2023-11-04 11:44:59 -0700

    fix migration

--- backend/orders/migrations/0010_remove_set_sizes_set_left_sizes_set_right_sizes.py
+++ backend/orders/migrations/0010_remove_set_sizes_set_left_sizes_set_right_sizes.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('orders', '0008_exampleimage_instagram_id'),
+        ('orders', '0009_alter_setimage_set'),
     ]
 
     operations = [
