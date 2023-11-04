6db8dabea3484150ba63490e0006b7fafd21cfda
jwily <johnlee1120@gmail.com>
2023-11-04 15:07:25 -0700

    ready to merge baos stuff

--- frontend/src/context/OrderContext.js
+++ frontend/src/context/OrderContext.js
@@ -19,7 +19,7 @@ const initialState = {
 };
 
 // Define a reducer function to manage state changes
-const reducer = (state=initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_NAME':
       console.log("Setting name to:", action.payload);
@@ -41,20 +41,21 @@ const reducer = (state=initialState, action) => {
         shape: '',
         photo: [],
         description: '',
-        extra: '' }
+        extra: ''
+      }
       return newState;
-      case 'INITIALIZE_STATE':
-        // Load initial state from localStorage if it exists
-        const savedState = localStorage.getItem('orderState');
-        return savedState ? JSON.parse(savedState) : initialState;
-      case 'SAVE_STATE':
-        // Save the state to localStorage
-        localStorage.setItem('orderState', JSON.stringify(state));
-        return state;
-        case 'CLEAR_LOCAL_STORAGE':
-        // Clear the localStorage
-        localStorage.clear();
-        return { ...initialState }; // Reset state to its initial state
+    case 'INITIALIZE_STATE':
+      // Load initial state from localStorage if it exists
+      const savedState = localStorage.getItem('orderState');
+      return savedState ? JSON.parse(savedState) : initialState;
+    case 'SAVE_STATE':
+      // Save the state to localStorage
+      localStorage.setItem('orderState', JSON.stringify(state));
+      return state;
+    case 'CLEAR_LOCAL_STORAGE':
+      // Clear the localStorage
+      localStorage.clear();
+      return { ...initialState }; // Reset state to its initial state
 
     // case 'CLEAR_FORM':
     //         console.log("Clearing the form");
@@ -69,27 +70,27 @@ export const OrderProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(reducer, initialState);
   const [image, setImage] = useState([])
-    // Dispatch the 'INITIALIZE_STATE' action to load the state from localStorage
-    useEffect(() => {
-      dispatch({ type: 'INITIALIZE_STATE' });
-    }, []);
+  // Dispatch the 'INITIALIZE_STATE' action to load the state from localStorage
+  useEffect(() => {
+    dispatch({ type: 'INITIALIZE_STATE' });
+  }, []);
 
-    useEffect(() => {
-      const grabImage = async() => {
-        const res = await fetch('/api/exampleimages/')
+  useEffect(() => {
+    const grabImage = async () => {
+      const res = await fetch('/api/exampleimages/')
 
-        if(res.ok){
-          const pic = await res.json()
-          setImage(pic)
-          console.log('it worked');
-          console.log('from the fetch',pic);
-        }
-        else{
-          console.log('it didnt work');
-        }
+      if (res.ok) {
+        const pic = await res.json()
+        setImage(pic)
+        console.log('it worked');
+        console.log('from the fetch', pic);
       }
-      grabImage()
-    }, [])
+      else {
+        console.log('it didnt work');
+      }
+    }
+    grabImage()
+  }, [])
 
   // Save the state to localStorage whenever it changes
   useEffect(() => {
@@ -103,7 +104,7 @@ export const OrderProvider = ({ children }) => {
   const scrollToFAQ = useRef()
 
   return (
-    <OrderContext.Provider value={{image, state, dispatch, scrollToOrder, scrollToAbout, scrollToGallery, scrollToFAQ }}>
+    <OrderContext.Provider value={{ image, state, dispatch, scrollToOrder, scrollToAbout, scrollToGallery, scrollToFAQ }}>
       {children}
     </OrderContext.Provider>
   );
