98a09651e6e84a959c8ea3cadb0f78bec03e6bf8
Melody Yoo <melodyyoo72@gmail.com>
2023-11-04 22:55:32 -0700

    style: add basic styling to EditSetForm

--- frontend/src/components/OrderSetForm/EditSetForm.js
+++ frontend/src/components/OrderSetForm/EditSetForm.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
-import { useOrderContext } from '../../context/OrderContext';
-import LoadingPage from '../LoadingPage';
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { useOrderContext } from "../../context/OrderContext";
+import LoadingPage from "../LoadingPage";
 
 const EditSetForm = () => {
   const history = useHistory();
@@ -10,20 +10,20 @@ const EditSetForm = () => {
   const { index } = useParams();
   const setIndex = parseInt(index, 10);
   const [isLoading, setIsLoading] = useState(true); // Initialize the loading state
-  const isOrderDetailsComplete = state.name && state.email
+  const isOrderDetailsComplete = state.name && state.email;
 
-  const [editedTier, setEditedTier] = useState('');
-  const [editedShape, setEditedShape] = useState('');
-  const [editedLeftDisplay, setEditedLeftDisplay] = useState('');
-  const [editedRightDisplay, setEditedRightDisplay] = useState('');
-  const [editedPhoto, setEditedPhoto] = useState('');
-  const [editedDescription, setEditedDescription] = useState('');
+  const [editedTier, setEditedTier] = useState("");
+  const [editedShape, setEditedShape] = useState("");
+  const [editedLeftDisplay, setEditedLeftDisplay] = useState("");
+  const [editedRightDisplay, setEditedRightDisplay] = useState("");
+  const [editedPhoto, setEditedPhoto] = useState("");
+  const [editedDescription, setEditedDescription] = useState("");
   const [editedExtra, setEditedExtra] = useState(0);
   const [editedExtra2, setEditedExtra2] = useState(0);
 
   const redirectToOrderDetails = () => {
-    window.location.href = '/order'
-  }
+    window.location.href = "/order";
+  };
 
   useEffect(() => {
     // Simulate loading for 100 milliseconds (0.1 seconds) and then set loading to false
@@ -55,19 +55,18 @@ const EditSetForm = () => {
       description: editedDescription,
       extra: editedExtra,
       extra2: editedExtra2,
-
     };
 
     const updatedSets = [...sets];
     updatedSets[setIndex] = updatedSet;
 
-    dispatch({ type: 'UPDATE_SETS', payload: updatedSets });
+    dispatch({ type: "UPDATE_SETS", payload: updatedSets });
 
-    history.push('/review-order');
+    history.push("/review-order");
   };
 
   if (!dataResult) {
-    return <LoadingPage />
+    return <LoadingPage />;
   }
   return (
     <>
@@ -80,13 +79,16 @@ const EditSetForm = () => {
               <div className="accordion-group">
                 <div className="accordion">
                   <input type="checkbox" id="accordion-2" className="accordion-toggle" />
-                  <label htmlFor="accordion-2" className="accordion-title bg-red-100">Tier</label>
+                  <label htmlFor="accordion-2" className="accordion-title bg-red-100">
+                    Tier
+                  </label>
                   <div className="accordion-content">
                     <div className="min-h-0">
                       {dataResult.map((tierOption) => (
                         <div key={tierOption.id}>
-                          <label>
+                          <div className="flex">
                             <input
+                              className="radio-solid-error radio mr-2 mt-1"
                               type="radio"
                               name="tier"
                               value={tierOption.name}
@@ -94,22 +96,27 @@ const EditSetForm = () => {
                               onChange={() => setEditedTier(tierOption.name)} // Set editedTier on change
                               required
                             />
-                            {tierOption.name}
-                            <span> ${tierOption.price} </span>
-                            <p>{tierOption.description}</p>
-                          </label>
+                            <span className="font-bold text-lg ml-1">{tierOption.name}: </span>
+                            <span className="font-bold text-lg ml-1"> ${tierOption.price} </span>
+                          </div>
+                          <p>{tierOption.description}</p>
                         </div>
                       ))}
                     </div>
                   </div>
                   <div className="accordion">
                     <input type="checkbox" id="accordion-1" className="accordion-toggle" />
-                    <label htmlFor="accordion-1" className="accordion-title bg-red-100">Shape</label>
+                    <label htmlFor="accordion-1" className="accordion-title bg-red-100">
+                      Shape
+                    </label>
                     <div className="accordion-content">
                       <div className="min-h-0">
-                        <label>Shape:</label>
-                        <select value={editedShape} onChange={(e) => setEditedShape(e.target.value)}>
-                          <option value="Extra-Short Square" >Extra-Short Square</option>
+                        <select
+                          className="bg-white select text-black"
+                          value={editedShape}
+                          onChange={(e) => setEditedShape(e.target.value)}
+                        >
+                          <option value="Extra-Short Square">Extra-Short Square</option>
                           <option value="Short Square">Short Square</option>
                           <option value="Medium Square">Medium Square</option>
                           <option value="Short Coffin">Short Coffin</option>
@@ -123,42 +130,47 @@ const EditSetForm = () => {
                           <option value="Medium Stiletto">Medium Stiletto</option>
                         </select>
                       </div>
-
                     </div>
                   </div>
                   <div className="accordion">
                     <input type="checkbox" id="accordion-3" className="accordion-toggle" />
-                    <label htmlFor="accordion-3" className="accordion-title bg-red-100">LeftDisplay</label>
+                    <label htmlFor="accordion-3" className="accordion-title bg-red-100">
+                      Left Display
+                    </label>
                     <div className="accordion-content">
                       <div className="min-h-0">
-                        <label>Left Display</label>
                         <input
                           type="text"
                           value={editedLeftDisplay}
                           onChange={(e) => setEditedLeftDisplay(e.target.value)}
+                          className="bg-white input text-black"
+                          placeholder="Ex. 2,6,7,9,6"
                         />
                       </div>
-
                     </div>
                   </div>
                   <div className="accordion">
                     <input type="checkbox" id="accordion-4" className="accordion-toggle" />
-                    <label htmlFor="accordion-4" className="accordion-title bg-red-100">Right Display</label>
+                    <label htmlFor="accordion-4" className="accordion-title bg-red-100">
+                      Right Display
+                    </label>
                     <div className="accordion-content">
                       <div className="min-h-0">
-                        <label>Right Display</label>
                         <input
                           type="text"
                           value={editedRightDisplay}
                           onChange={(e) => setEditedRightDisplay(e.target.value)}
+                          className="bg-white input text-black"
+                          placeholder="Ex. 2,6,7,9,6"
                         />
                       </div>
-
                     </div>
                   </div>
                   <div className="accordion">
                     <input type="checkbox" id="accordion-5" className="accordion-toggle" />
-                    <label htmlFor="accordion-5" className="accordion-title bg-red-100">Photo</label>
+                    <label htmlFor="accordion-5" className="accordion-title bg-red-100">
+                      Photo
+                    </label>
                     <div className="accordion-content">
                       <div className="min-h-0">
                         <label>Photo:</label>
@@ -172,56 +184,72 @@ const EditSetForm = () => {
                   </div>
                   <div className="accordion">
                     <input type="checkbox" id="accordion-6" className="accordion-toggle" />
-                    <label htmlFor="accordion-6" className="accordion-title bg-red-100">Description</label>
+                    <label htmlFor="accordion-6" className="accordion-title bg-red-100">
+                      Description
+                    </label>
                     <div className="accordion-content">
                       <div className="min-h-0">
-                        <label>Description:</label>
                         <textarea
                           value={editedDescription}
                           onChange={(e) => setEditedDescription(e.target.value)}
+                          className="bg-white textarea textarea-block textarea-solid text-black"
+                          style={{ resize: "none", height: "100px" }}
                         />
                       </div>
-
                     </div>
                   </div>
                   <div className="accordion">
                     <input type="checkbox" id="accordion-7" className="accordion-toggle" />
-                    <label htmlFor="accordion-7" className="accordion-title bg-red-100">Charm</label>
+                    <label htmlFor="accordion-7" className="accordion-title bg-red-100">
+                      Charm(s)
+                    </label>
                     <div className="accordion-content">
                       <div className="min-h-0">
-                        <label>charm:</label>
+                        <label>Charm count: </label>
                         <input
                           type="number"
                           value={editedExtra}
-                          onChange={(e) => setEditedExtra(e.target.value)}
+                          onChange={(e) => {
+                            if (e.target.value <= 25 && e.target.value >= 0) {
+                              setEditedExtra(e.target.value);
+                            }
+                          }}
+                          className="input bg-white text-black"
+                          max='25'
+                          min='0'
                         />
                       </div>
-
                     </div>
                   </div>
                   <div className="accordion">
                     <input type="checkbox" id="accordion-8" className="accordion-toggle" />
-                    <label htmlFor="accordion-8" className="accordion-title bg-red-100">Character</label>
+                    <label htmlFor="accordion-8" className="accordion-title bg-red-100">
+                      Character(s)
+                    </label>
                     <div className="accordion-content">
                       <div className="min-h-0">
-                        <label>character:</label>
+                        <label>Character count: </label>
                         <input
                           type="number"
                           value={editedExtra2}
-                          onChange={(e) => setEditedExtra2(e.target.value)}
+                          onChange={(e) => {
+                            if (e.target.value <= 25 && e.target.value >= 0) {
+                              setEditedExtra2(e.target.value);
+                            }
+                          }}
+                          className="input bg-white text-black"
+                          max='25'
+                          min='0'
                         />
                       </div>
-
                     </div>
                   </div>
                 </div>
               </div>
-              <div>
-                <button onClick={handleSaveSet}>Save</button>
-                <div></div>
-                <button onClick={() => history.push('/review-order')}>Cancel</button>
+              <div className="flex gap-3 m-7">
+                <button className='rounded-lg btn btn-primary btn-block bg-primary_blue text-black' onClick={() => history.push("/review-order")}>Cancel</button>
+                <button className='mb-7 rounded-lg btn btn-primary btn-block bg-primary_blue text-black' onClick={handleSaveSet}>Save</button>
               </div>
-
             </div>
           ) : (
             <div>
@@ -232,7 +260,6 @@ const EditSetForm = () => {
         </>
       )}
     </>
-
   );
 };
 
