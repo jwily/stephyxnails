da96de1c1cec0b1aba5d0d64346a5add47ce22f5
Melody Yoo <melodyyoo72@gmail.com>
2023-11-04 19:38:10 -0700

    style: basic styling for ExtraForm

--- frontend/src/components/OrderSetForm/6_ExtraForm.js
+++ frontend/src/components/OrderSetForm/6_ExtraForm.js
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { useOrderContext } from '../../context/OrderContext';
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useOrderContext } from "../../context/OrderContext";
 
 function ExtraForm() {
-
-  const history = useHistory()
+  const history = useHistory();
   const { state, dispatch } = useOrderContext();
   const [calculatedValue, setCalculatedValue] = useState(0);
   const [calculatedValue2, setCalculatedValue2] = useState(0);
-  const [extra, setExtra] = useState(state.formData.extra || '');
-  const [extra2, setExtra2] = useState(state.formData.extra2 || '');
+  const [extra, setExtra] = useState(state.formData.extra || "");
+  const [extra2, setExtra2] = useState(state.formData.extra2 || "");
   const [isLoading, setIsLoading] = useState(true); // Initialize the loading state
-  const isOrderDetailsComplete = state.name && state.email
-  const [error, setError] = useState(''); // State to hold error message
-  const [error2, setError2] = useState(''); // State to hold error message
+  const isOrderDetailsComplete = state.name && state.email;
+  const [error, setError] = useState(""); // State to hold error message
+  const [error2, setError2] = useState(""); // State to hold error message
 
+  console.log("EXTRA: ", parseInt(extra));
 
   const redirectToOrderDetails = () => {
-    window.location.href = '/order'
-  }
+    window.location.href = "/order";
+  };
 
   useEffect(() => {
     // Simulate loading for 100 milliseconds (0.1 seconds) and then set loading to false
@@ -30,14 +30,14 @@ function ExtraForm() {
 
   // Load 'extra' data from localStorage when the component mounts
   useEffect(() => {
-    const savedExtra = localStorage.getItem('extra');
+    const savedExtra = localStorage.getItem("extra");
     if (savedExtra) {
       setExtra(parseInt(savedExtra, 10));
     }
   }, []);
 
   useEffect(() => {
-    const savedExtra2 = localStorage.getItem('extra2');
+    const savedExtra2 = localStorage.getItem("extra2");
     if (savedExtra2) {
       setExtra2(parseInt(savedExtra2, 10));
     }
@@ -45,23 +45,22 @@ function ExtraForm() {
 
   // Save 'extra' data to localStorage when 'extra' changes
   useEffect(() => {
-    localStorage.setItem('extra', extra.toString());
+    localStorage.setItem("extra", extra.toString());
   }, [extra]);
 
   useEffect(() => {
-    localStorage.setItem('extra2', extra2.toString());
+    localStorage.setItem("extra2", extra2.toString());
   }, [extra2]);
-
 
   // Load 'calculatedValue' from localStorage when the component mounts
   useEffect(() => {
-    const savedCalculatedValue = localStorage.getItem('calculatedValue');
+    const savedCalculatedValue = localStorage.getItem("calculatedValue");
     if (savedCalculatedValue) {
       setCalculatedValue(parseInt(savedCalculatedValue, 10));
     }
   }, []);
   useEffect(() => {
-    const savedCalculatedValue2 = localStorage.getItem('calculatedValue2');
+    const savedCalculatedValue2 = localStorage.getItem("calculatedValue2");
     if (savedCalculatedValue2) {
       setCalculatedValue2(parseInt(savedCalculatedValue2, 10));
     }
@@ -72,64 +71,75 @@ function ExtraForm() {
     setCalculatedValue(extra * 5);
 
     // Save the updated 'calculatedValue' to localStorage whenever it changes
-    localStorage.setItem('calculatedValue', calculatedValue.toString());
+    localStorage.setItem("calculatedValue", calculatedValue.toString());
   }, [extra, calculatedValue]);
 
   useEffect(() => {
     setCalculatedValue2(extra2 * 10);
 
     // Save the updated 'calculatedValue' to localStorage whenever it changes
-    localStorage.setItem('calculatedValue', calculatedValue2.toString());
+    localStorage.setItem("calculatedValue", calculatedValue2.toString());
   }, [extra2, calculatedValue2]);
 
   const handleNext = (e) => {
     e.preventDefault();
-    dispatch({ type: 'UPDATE_FORM_DATA', payload: { extra, extra2 } });
-    history.push('/order-set/currentset'); // Navigate to the next form question
+    dispatch({ type: "UPDATE_FORM_DATA", payload: { extra, extra2 } });
+    history.push("/order-set/currentset"); // Navigate to the next form question
   };
 
   const handleBack = () => {
     // Navigate back to the previous step
-    history.push('/order-set/description');
+    history.push("/order-set/description");
   };
 
-  const handleInputChange = (e) => {
-    const newValue = parseInt(e.target.value, 10);
+  // const handleInputChange = (e) => {
+  //   const newValue = parseInt(e.target.value, 10);
 
-    if (!isNaN(newValue)) {
-      if (newValue < 0) {
-        setExtra(0);
-        setError('charm cannot be negative.');
-      } else if (newValue > 25) {
-        setExtra(25);
-        setError('charm cannot exceed 25.');
-      } else {
-        setExtra(newValue);
-        setError('');
-      }
-    } else {
-      setError('Please enter a valid number.');
+  //   if (!isNaN(newValue)) {
+  //     if (newValue < 0) {
+  //       setExtra(0);
+  //       setError("charm cannot be negative.");
+  //     } else if (newValue > 25) {
+  //       setExtra(25);
+  //       setError("charm cannot exceed 25.");
+  //     } else {
+  //       setExtra(newValue);
+  //       setError("");
+  //     }
+  //   } else {
+  //     setError("Please enter a valid number.");
+  //   }
+  // };
+
+  // const handleInputChange2 = (e) => {
+  //   const newValue2 = parseInt(e.target.value, 10);
+
+  //   if (!isNaN(newValue2)) {
+  //     if (newValue2 < 0) {
+  //       setExtra2(0);
+  //       setError2("character cannot be negative.");
+  //     } else if (newValue2 > 25) {
+  //       setExtra2(25);
+  //       setError2("character cannot exceed 25.");
+  //     } else {
+  //       setExtra2(newValue2);
+  //       setError2("");
+  //     }
+  //   } else {
+  //     setError2("Please enter a valid number.");
+  //   }
+  // };
+
+  const numericalOptions = () => {
+    const options = [];
+    for (let i = 0; i < 26; i++) {
+      options.push(
+        <option key={i} value={i + 1}>
+          {i + 1}
+        </option>
+      );
     }
-  };
-
-
-  const handleInputChange2 = (e) => {
-    const newValue2 = parseInt(e.target.value, 10);
-
-    if (!isNaN(newValue2)) {
-      if (newValue2 < 0) {
-        setExtra2(0);
-        setError2('character cannot be negative.');
-      } else if (newValue2 > 25) {
-        setExtra2(25);
-        setError2('character cannot exceed 25.');
-      } else {
-        setExtra2(newValue2);
-        setError2('');
-      }
-    } else {
-      setError2('Please enter a valid number.');
-    }
+    return options;
   };
 
   return (
@@ -138,35 +148,64 @@ function ExtraForm() {
         <div>Loading...</div>
       ) : (
         <>
+
           {isOrderDetailsComplete ? (
             <section className="bg-primary rounded-xl m-4">
               <div className="p-8 shadow-lg">
                 <form className="space-y-4">
                   <div className="w-full">
+                    <h1 className="font-extrabold text-xl text-center mb-4">6. Choose Charm/Character Add Ons</h1>
                     <div>Charms</div>
-                    <label className="sr-only" htmlFor="name">Name</label>
-                    <input className="input input-solid max-w-full bg-white text-black" placeholder="" type="number" id="number" value={extra} onChange={handleInputChange} />
-                    {error && <p style={{ color: 'red' }}>{error}</p>}
+                    <label className="sr-only" htmlFor="name">
+                      Name
+                    </label>
+                    <select
+                      className="input input-solid max-w-full bg-white text-black"
+                      placeholder=""
+                      type="number"
+                      id="number"
+                      value={extra}
+                      onChange={(e) => setExtra(e.target.value)}
+                    >
+                      {numericalOptions()}
+                    </select>
+                    {error && <p style={{ color: "red" }}>{error}</p>}
                     <div>$5 per Charm = ${calculatedValue}</div>
                   </div>
 
                   <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
                     <div>
                       <div>Characters</div>
-                      <label className="sr-only" htmlFor="email">Email</label>
-                      <input className="input input-solid bg-white text-black" placeholder="" type="number" id="number" value={extra2} onChange={handleInputChange2} />
-                      {error2 && <p style={{ color: 'red' }}>{error2}</p>}
+                      <label className="sr-only" htmlFor="email">
+                        Email
+                      </label>
+                      <select
+                        className="input input-solid bg-white text-black"
+                        placeholder=""
+                        type="number"
+                        id="number"
+                        value={extra2}
+                        onChange={(e) => setExtra2(e.target.value)}
+                      >
+                        {numericalOptions()}
+                      </select>
+                      {error2 && <p style={{ color: "red" }}>{error2}</p>}
                       <div>$10 per Character = ${calculatedValue2}</div>
                     </div>
                   </div>
 
-                  <div className="mt-4 flex justify-around">
-                    <button type="button" className="rounded-lg btn btn-primary" onClick={handleBack}>Back</button>
-                    <button className="rounded-lg btn btn-primary" type="submit" onClick={handleNext}>Next</button>
+                  <div className="flex gap-3 mt-7">
+                    <button type="button" className="rounded-lg btn btn-primary btn-block bg-primary_blue text-black" onClick={handleBack}>
+                    ←
+                    </button>
+                    <button className="rounded-lg btn btn-primary btn-block bg-primary_blue text-black" type="submit" onClick={handleNext}>
+                    →
+                    </button>
                   </div>
                 </form>
               </div>
             </section>
+          ) : (
             // <section>
             // <h2>5. Charms</h2>
             // <p>disclaimer insert</p>
@@ -203,9 +242,6 @@ function ExtraForm() {
             // </div>
             // </section>
 
-
-
-          ) : (
             <div>
               <p>Please complete your order details before proceeding.</p>
               <button onClick={redirectToOrderDetails}>Complete Order Details</button>
