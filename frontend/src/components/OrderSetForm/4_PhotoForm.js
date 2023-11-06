3a72025f17a8e1e4322deae852e6c1a33f61b7b2
Melody Yoo <melodyyoo72@gmail.com>
2023-11-05 19:08:36 -0800

    style: decrease margin top for photoform button

--- frontend/src/components/OrderSetForm/4_PhotoForm.js
+++ frontend/src/components/OrderSetForm/4_PhotoForm.js
@@ -133,7 +133,7 @@ function PhotoForm() {
                     </div>
                   </div>
                 </div>
-                <div className="flex gap-3 mt-7">
+                <div className="flex gap-3 mt-3">
                   <button className="rounded-lg btn btn-primary btn-block bg-primary_blue text-black" onClick={handleBack}>←</button>
                   <button className="rounded-lg btn btn-primary btn-block bg-primary_blue text-black" type="submit" onClick={handleNext}>→</button>
                 </div>
